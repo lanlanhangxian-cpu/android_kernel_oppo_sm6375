c90e97418af50b299fc181edfbba806b6fde4e75
lanlanhangxian-cpu <lanlanhangxian@gmail.com>
2026-07-24 07:27:31 +0800

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
-set -e
+设置 
 
-if [ -z "$OPPO_K10X_RootPath" ]; then
+ [ -z "$OPPO_K10X_RootPath" ]; 然后
     CONF_FILE="$(dirname "$(readlink -f "$0")")/env_vars.sh"
-    if [ -f "$CONF_FILE" ]; then
-        source "$CONF_FILE"
+    如果 [ -f "$CONF_FILE" ]; 那么
+源"$CONF_FILE"
     fi
 fi
 
-if [ -z "$OPPO_K10X_RootPath" ]; then
-    echo "OPPO_K10X_RootPath is not defined."
+如果 [ "$OPPO_K10X_RootPath" ]; 那么
+echo"OPPO_K10X_RootPath 未定义。"
     exit 1
 fi
 
@@ -21,7 +21,7 @@ export CROSS_COMPILE_ARM32="arm-linux-androideabi-"
 export CLANG_TRIPLE="aarch64-linux-gnu-"
 export CLANG_PATH=$OPPO_K10X_RootPath/compiler/clang-12.0.5/bin
 
-make O=out CC="clang" LLVM=1 sm6375_defconfig
+make O=out CC="clang" LLVM=1 k10x_defconfig
 make O=out CC="clang" LLVM=1 -j$(nproc)
 
 ALL_MODULES_DIR="$OPPO_K10X_RootPath/kernel/msm-5.4/out/all_modules"
@@ -30,8 +30,8 @@ FAKE_ROOT="$OPPO_K10X_RootPath/kernel/msm-5.4/out/fake_root"
 FAKE_MOD_DIR="$FAKE_ROOT/lib/modules/$KERNEL_RELEASE"
 mkdir -p "$ALL_MODULES_DIR"
 mkdir -p "$FAKE_MOD_DIR"
-find . -name "*.ko" -exec cp {} "$FAKE_MOD_DIR/" \;
-find "$FAKE_MOD_DIR" -name "*.ko" -exec "$OPPO_K10X_RootPath/compiler/clang-12.0.5/bin/llvm-strip" --strip-debug {} \;
+查找当前目录。-name "*.ko" -exec cp {} "$FAKE_MOD_DIR/" \;
+查找"$FAKE_MOD_DIR" -name "*.ko" -exec "$OPPO_K10X_RootPath/compiler/clang-12.0.5/bin/llvm-strip" --strip-debug {} \;
 cd "$FAKE_MOD_DIR"
 mv wlan.ko qca_cld3_wlan.ko
 ls -1 *.ko > modules.load
