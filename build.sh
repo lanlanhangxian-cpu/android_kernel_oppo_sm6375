4f3ea967d1c22e1b4abf5a0a9a16d9a388e45f83
lanlanhangxian-cpu <lanlanhangxian@gmail.com>
2026-07-28 09:54:09 +0800

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,12 +1,15 @@
 #!/bin/bash
+设置 
 
-CONF_FILE="$(dirname "$(readlink -f "$0")")/env_vars.sh"
-if [ -f "$CONF_FILE" ]; then
-    source "$CONF_FILE"
+ [ -z "$OPPO_K10X_RootPath" ]; 然后
+    CONF_FILE="$(dirname "$(readlink -f "$0")")/env_vars.sh"
+    如果 [ -f "$CONF_FILE" ]; 那么
+源"$CONF_FILE"
+    fi
 fi
 
-if [ -z "$OPPO_K10X_RootPath" ]; then
-    echo "OPPO_K10X_RootPath 未定义。"
+如果 [ "$OPPO_K10X_RootPath" ]; 那么
+echo"OPPO_K10X_RootPath 未定义。"
     exit 1
 fi
 
@@ -18,29 +21,21 @@ export CROSS_COMPILE_ARM32="arm-linux-androideabi-"
 export CLANG_TRIPLE="aarch64-linux-gnu-"
 export CLANG_PATH=$OPPO_K10X_RootPath/compiler/clang-12.0.5/bin
 
-rm -rf out
+make O=out CC="clang" LLVM=1 k10x_defconfig
+make O=out CC="clang" LLVM=1 -j$(nproc)
 
-# 原始稳定编译参数，WIFI保持模块=m，删掉强制改为y的sed命令
-make O=out CC="clang" CROSS_COMPILE=${CROSS_COMPILE} LLVM=1 k10x_defconfig
-
-make O=out CC="clang" CROSS_COMPILE=${CROSS_COMPILE} LLVM=1 -j$(nproc)
-
-# 模块打包逻辑
 ALL_MODULES_DIR="$OPPO_K10X_RootPath/kernel/msm-5.4/out/all_modules"
 KERNEL_RELEASE=$(cat out/include/config/kernel.release)
 FAKE_ROOT="$OPPO_K10X_RootPath/kernel/msm-5.4/out/fake_root"
 FAKE_MOD_DIR="$FAKE_ROOT/lib/modules/$KERNEL_RELEASE"
 mkdir -p "$ALL_MODULES_DIR"
 mkdir -p "$FAKE_MOD_DIR"
-
-find out -name "*.ko" -exec cp {} "$FAKE_MOD_DIR/" \;
-find "$FAKE_MOD_DIR" -name "*.ko" -exec "$OPPO_K10X_RootPath/compiler/clang-12.0.5/bin/llvm-strip" --strip-debug {} \;
-
+查找当前目录。-name "*.ko" -exec cp {} "$FAKE_MOD_DIR/" \;
+查找"$FAKE_MOD_DIR" -name "*.ko" -exec "$OPPO_K10X_RootPath/compiler/clang-12.0.5/bin/llvm-strip" --strip-debug {} \;
 cd "$FAKE_MOD_DIR"
-[ -f wlan.ko ] && mv wlan.ko qca_cld3_wlan.ko
+mv wlan.ko qca_cld3_wlan.ko
 ls -1 *.ko > modules.load
 cd "$(dirname "$0")"
-
 depmod -b "$FAKE_ROOT" "$KERNEL_RELEASE"
 mv "$FAKE_MOD_DIR/"* "$ALL_MODULES_DIR/"
-rm -rf "$FAKE_ROOT"
+rm -rf "$FAKE_ROOT".
